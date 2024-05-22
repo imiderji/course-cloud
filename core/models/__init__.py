57c9898b6c11341ae5566c75530de04c92b1527d
skishou <skishou.developer@yandex.ru>
2024-05-22 17:01:10 +0300

    Реализован класс для подключения к бд

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,9 +1,15 @@
 __all__ = (
     "Base",
     "Berth",
+    "Dock",
+    "RelationDockBerth",
+    "Ship",
     "Shipowner",
 )
 
 from .base import Base
 from .berth import Berth
+from .dock import Dock
+from .relation_dock_berth import RelationDockBerth
+from .ship import Ship
 from .shipowner import Shipowner
